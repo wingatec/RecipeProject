b769c2f9a5b0e02840d2d208c12591aa6339291f
wingatec <carmelitawingate@gmail.com>
2014-07-30 16:47:11 -0400

    NewMvc
    created RecipeMvc2,, generated Models, connected tables to Mvc :-)

---
+++ RecipeMvc2/RecipeMvc2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecipeMvc2.Startup))]
+namespace RecipeMvc2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
